af75ec4e943004085eedaeac179cae4ca602213b
Tom Gernez <tomgernez@Toms-MacBook-Pro-2.local>
2025-07-26 11:22:44 +0200

    change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "v8n",
+  description: "Build n8n agents with natural language",
 };
 
 const geistSans = Geist({
